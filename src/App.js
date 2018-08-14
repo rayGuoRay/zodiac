f3a20148298c96ee34ca36fd76d3c0489c765827
guolei <guolei_ray@163.com>
2018-08-14 19:42:25 +0800

    [Project] perfect weather component
    Signed-off-by: guolei <guolei_ray@163.com>

--- src/App.js
+++ src/App.js
@@ -7,18 +7,14 @@ import SignComponent from './component/sign/SignComponent';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      <div>
         <NavigationComponent title="ray test"/>
         <SignComponent />
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/component/navigation/NavigationWeatherComponent.js
+++ src/component/navigation/NavigationWeatherComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './NavigationWeatherComponent.css'
+import './NavigationWeatherComponent.css';
 import comfortableImg from '../../img/comfortable_temperature.png';
 import unComfortableImg from '../../img/uncomfortable_temperature.png';
 import suitableWashCarImg from '../../img/suitable_car_washing.png';
@@ -16,6 +16,8 @@ import unFitTravImg from '../../img/unfit_trav.png';
 import uvHighImg from '../../img/uv_high.png';
 import uvNormalImg from '../../img/uv_normal.png';
 import uvLowImg from '../../img/uv_low.png';
+
+require ('./NavigationWeatherIconFont.js');
 
 class NavigationWeatherComponent extends Component {
 
@@ -34,36 +36,50 @@ class NavigationWeatherComponent extends Component {
     } 
 
     render() {
+        const location = "成都:";
         const cond_image = "https://cdn.heweather.com/cond_icon/" + this.state.weatherInfo.cond_code + ".png";
-        const tmp_value = this.state.weatherInfo.tmp;
-        const comfBrf = this.state.comfInfo.brf;
-        const comBrfIcon = comfBrf && comfBrf.indexOf("不舒适") == -1 ? comfortableImg : unComfortableImg;
-        const drsgBrf = this.state.drsgInfo.brf;
-        const drsgBrfIcon = drsgBrf && drsgBrf.indexOf("舒适") == -1 ? (drsgBrf.indexOf("热") == -1 ? drsgColdImg : drsgHotImg) : drsgFitImg;
-        const fluBrf = this.state.fluInfo.brf;
-        const fluBrfIcon = fluBrf && fluBrf.indexOf("易发") == -1 ? fluRiskLowImg : fluRiskHighImg;
-        const sportBrf = this.state.sportInfo.brf;
-        const sportBrfIcon = sportBrf && sportBrf.indexOf("适宜") == -1 ? unFitSportImg : fitSportImg; 
-        const travBrf = this.state.travInfo.brf;
-        const travBrfIcon = travBrf && travBrf.indexOf("适宜") == -1 ? unFitTravImg : fitTravImg;
-        const uvBrf = this.state.uvInfo.brf;
-        const uvBrfIcon = uvBrf && uvBrf.indexOf("中等") == -1 ? (uvBrf.indexOf("强") == -1 ? uvLowImg : uvHighImg) : uvNormalImg; 
-        const cwBrf = this.state.cwInfo.brf;
-        const cwBrfIcon = cwBrf && cwBrf.indexOf("不宜") === -1 ? suitableWashCarImg : unSuitableWashCarImg;
+        const tmp_value = this.state.weatherInfo.tmp + "℃";
+        const fitColorStyle = {fill:'#1296DB'};
+        const unFitColorStyle = {fill:'#FF6C6C'}
+        // clothing index
+        const isDrsgUnFit = this.state.drsgInfo.brf && this.state.drsgInfo.brf.indexOf("舒适") == -1;
+        const isDrsgCold = this.state.drsgInfo.brf && this.state.drsgInfo.brf.indexOf("热") == -1;
+        const drsgBrfIcon = isDrsgUnFit ? (isDrsgCold ? "#icon-dayi" : "#icon-duanxiu") : "#icon-changxiu";
+        const drsgIconColorStyle= isDrsgUnFit ? (isDrsgCold ? {fill:'#1296DB'} : {fill:'#FF6C6C'}) : {fill:'#f39813'}
+        
+        // const fluBrf = this.state.fluInfo.brf;
+        // const fluBrfIcon = fluBrf && fluBrf.indexOf("易发") == -1 ? fluRiskLowImg : fluRiskHighImg;
+        
+        const isSportUnFit = this.state.sportInfo.brf && this.state.sportInfo.brf.indexOf("适宜") == -1
+        const sportIconSyle = isSportUnFit ? unFitColorStyle : fitColorStyle;
+
+        const isTravUnFit = this.state.travInfo.brf && this.state.travInfo.brf.indexOf("适宜") == -1;
+        const travIconStyle = isTravUnFit ? unFitColorStyle : fitColorStyle;
+        
+        // const uvBrf = this.state.uvInfo.brf;
+        // const uvBrfIcon = uvBrf && uvBrf.indexOf("中等") == -1 ? (uvBrf.indexOf("强") == -1 ? uvLowImg : uvHighImg) : uvNormalImg; 
+        
+        const isCwFit = this.state.cwInfo.brf && this.state.cwInfo.brf.indexOf("不宜") === -1;
+        const cwIconColorStyle = isCwFit ? fitColorStyle : unFitColorStyle;
 
         return (
             <div className='navigationWeatherStyle'>
-                <div className="navigationWeatherLocationStyle">成都:</div>
+                <div className="navigationWeatherLocationFontStyle">{location}</div>
+                <div className="navigationWeatherTemperatureFontStyle">{tmp_value}</div>
                 <img className="navigationWeatherIconStyle" src={cond_image}/>
-                <div className="navigationWeatherTemperatureStyle"> {tmp_value}℃ </div>
-                <img className="navigationWeatherIconStyle" src={comBrfIcon}/>
-                <img className="navigationWeatherIconStyle" src={drsgBrfIcon}/>
-                <img className="navigationWeatherIconStyle" src={fluBrfIcon}/>
-                <img className="navigationWeatherIconStyle" src={sportBrfIcon}/>
-                <img className="navigationWeatherIconStyle" src={travBrfIcon}/>
-                <img className="navigationWeatherIconStyle" src={uvBrfIcon}/>       
-                <img className="navigationWeatherIconStyle" src={cwBrfIcon}/>
-            </div>    
+                <svg className="navigationLifeStyleIconStyle" aria-hidden="true" style={drsgIconColorStyle}>
+                    <use xlinkHref={drsgBrfIcon}></use>
+                </svg>
+                <svg className="navigationLifeStyleIconStyle" aria-hidden="true" style={sportIconSyle}>
+                    <use xlinkHref="#icon-yundong"></use>
+                </svg>
+                <svg className="navigationLifeStyleIconStyle" aria-hidden="true" style={travIconStyle}>
+                    <use xlinkHref="#icon-feiji"></use>
+                </svg>   
+                <svg className="navigationLifeStyleIconStyle" aria-hidden="true" style={cwIconColorStyle}>
+                    <use xlinkHref="#icon-xiche"></use>
+                </svg>
+            </div>
         );
     }
 
